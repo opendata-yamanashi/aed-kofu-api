6e87a3f75334cd3d94b4bfb55bb933e5ff841fe8
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-05-26 20:37:10 +0900

    APIに型を付与

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from data import Yamanashi_AED
 from fastapi import FastAPI
 
+from model import AED_Data
+from pydantic import BaseModel, Field
+from typing import List
+
 yA = Yamanashi_AED()
 yA.create_df()
 
@@ -10,15 +14,18 @@ app = FastAPI()
 def hello():
     return "Hello! Please access /docs"
 
-@app.get("/list/")
+@app.get("/list/", response_model=List[AED_Data])
 def get_data():
     return yA.df.to_dict("records")
 
-@app.get("/query/")
+@app.get("/query/", response_model=List[AED_Data])
 def do_query(q=None):
     return yA.query(q).to_dict("records")
 
-@app.get("/version/")
+class VersionEndpointResponse(BaseModel):
+    version: str = Field(None, alias="version")
+
+@app.get("/version/", response_model=VersionEndpointResponse)
 def get_version():
     return {"version": yA.get_data_version()}
 
